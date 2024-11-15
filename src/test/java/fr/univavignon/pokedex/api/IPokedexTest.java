50fbf78892255be40c16e7a909b480a3ca767b43
Hugo <chautardhugo@gmail.com>
2024-11-15 08:57:15 +0100

    Update Coverage

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -7,18 +7,26 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class IPokedexTest {
 
-    private IPokedex pokedexMock;
+    private IPokedex pokedex;
+    private IPokemonMetadataProvider metadataProviderMock;
+    private IPokemonFactory pokemonFactoryMock;
     private Pokemon pokemonMock;
 
     @BeforeEach
     public void setUp() {
-        // Mock de l'interface IPokedex
-        pokedexMock = mock(IPokedex.class);
+        // Mock des dépendances
+        metadataProviderMock = mock(IPokemonMetadataProvider.class);
+        pokemonFactoryMock = mock(IPokemonFactory.class);
         
+        // Initialisation de l'instance Pokedex avec des mocks
+        pokedex = new Pokedex(metadataProviderMock, pokemonFactoryMock);
+        
+        // Création d'un mock de Pokémon
         int index = 133;
         String name = "Aquali";
         int attack = 186;
@@ -35,34 +43,86 @@ public class IPokedexTest {
 
     @Test
     public void testSize() {
-        when(pokedexMock.size()).thenReturn(10);
+        assertEquals(0, pokedex.size(), "The Pokedex should initially be empty.");
 
-        assertEquals(10, pokedexMock.size(), "The size of the Pokedex should be 10.");
+        pokedex.addPokemon(pokemonMock);
+        assertEquals(1, pokedex.size(), "The Pokedex size should be 1 after adding a Pokemon.");
     }
 
     @Test
     public void testAddPokemon() {
-        when(pokedexMock.addPokemon(pokemonMock)).thenReturn(0);
-
-        assertEquals(0, pokedexMock.addPokemon(pokemonMock), "The index of the added Pokemon should be 0.");
+        int index = pokedex.addPokemon(pokemonMock);
+        assertEquals(0, index, "The index of the first added Pokemon should be 0.");
+        
+        int secondIndex = pokedex.addPokemon(pokemonMock);
+        assertEquals(1, secondIndex, "The index of the second added Pokemon should be 1.");
     }
 
     @Test
-    public void testGetPokemon() throws PokedexException {
-        when(pokedexMock.getPokemon(0)).thenReturn(pokemonMock);
+    public void testGetPokemonValidIndex() throws PokedexException {
+        pokedex.addPokemon(pokemonMock);
+        
+        Pokemon retrievedPokemon = pokedex.getPokemon(0);
+        assertNotNull(retrievedPokemon, "Pokemon at index 0 should not be null.");
+        assertEquals("Aquali", retrievedPokemon.getName(), "The retrieved Pokemon should have the name 'Aquali'.");
+    }
 
-        assertNotNull(pokedexMock.getPokemon(0), "Pokemon at index 0 should not be null.");
+    @Test
+    public void testGetPokemonInvalidIndex() {
+        assertThrows(PokedexException.class, () -> pokedex.getPokemon(0), 
+                     "Requesting a Pokemon with an invalid index should throw a PokedexException.");
     }
 
     @Test
     public void testGetPokemons() {
-        List<Pokemon> pokemons = new ArrayList<>();
-        pokemons.add(pokemonMock);
+        pokedex.addPokemon(pokemonMock);
 
-        when(pokedexMock.getPokemons()).thenReturn(pokemons);
+        List<Pokemon> retrievedPokemons = pokedex.getPokemons();
+        assertEquals(1, retrievedPokemons.size(), "The Pokedex should contain 1 Pokemon.");
+        assertEquals("Aquali", retrievedPokemons.get(0).getName(), "The Pokemon in the Pokedex should be 'Aquali'.");
 
-        assertNotNull(pokedexMock.getPokemons(), "The Pokedex should return a non-null list of Pokemons.");
-        assertEquals(1, pokedexMock.getPokemons().size(), "The Pokedex should contain 1 Pokemon.");
+        // Attempt to modify the returned list should throw an exception
+        assertThrows(UnsupportedOperationException.class, () -> retrievedPokemons.add(pokemonMock),
+                     "The returned Pokemon list should be unmodifiable.");
     }
 
+    @Test
+    public void testGetPokemonsWithOrder() {
+        // Adding multiple Pokemon to test ordering
+        Pokemon pokemon1 = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 100.0);
+        Pokemon pokemon2 = new Pokemon(1, "Bulbasaur", 118, 118, 90, 1115, 128, 2500, 3, 56.7);
+        pokedex.addPokemon(pokemon1);
+        pokedex.addPokemon(pokemon2);
+
+        // Sort by name in ascending order
+        List<Pokemon> sortedPokemons = pokedex.getPokemons(Comparator.comparing(Pokemon::getName));
+        
+        assertEquals("Aquali", sortedPokemons.get(0).getName(), "The first Pokemon should be 'Aquali' when sorted by name.");
+        assertEquals("Bulbasaur", sortedPokemons.get(1).getName(), "The second Pokemon should be 'Bulbasaur' when sorted by name.");
+    }
+
+    @Test
+    public void testGetPokemonMetadata() throws PokedexException {
+        // Mock the metadata provider to return specific metadata
+        PokemonMetadata metadata = new PokemonMetadata(133, "Aquali", 186, 168, 260);
+        when(metadataProviderMock.getPokemonMetadata(133)).thenReturn(metadata);
+
+        PokemonMetadata retrievedMetadata = pokedex.getPokemonMetadata(133);
+
+        assertNotNull(retrievedMetadata, "Metadata for Pokemon index 133 should not be null.");
+        assertEquals("Aquali", retrievedMetadata.getName(), "The metadata name should be 'Aquali'.");
+        assertEquals(186, retrievedMetadata.getAttack(), "The metadata attack should be 186.");
+    }
+
+    @Test
+    public void testCreatePokemon() {
+        // Mock the pokemon factory to return a specific Pokemon
+        when(pokemonFactoryMock.createPokemon(133, 2729, 202, 5000, 4)).thenReturn(pokemonMock);
+
+        Pokemon createdPokemon = pokedex.createPokemon(133, 2729, 202, 5000, 4);
+
+        assertNotNull(createdPokemon, "The created Pokemon should not be null.");
+        assertEquals("Aquali", createdPokemon.getName(), "The created Pokemon should have the name 'Aquali'.");
+        assertEquals(2729, createdPokemon.getCp(), "The created Pokemon should have a CP of 2729.");
+    }
 }
--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -51,4 +51,67 @@ public class IPokemonMetadataProviderTest {
         verify(mockPokemonMetadataProvider).getPokemonMetadata(PokemonEspece.getIndex());
         assertEquals(PokemonEspece, PokemonMetaData);
 	}
+	
+	@Test
+	public void validatePokemonMetadataEqualityTest_getDefense() throws PokedexException {
+	    PokemonMetadata pokemonEspece = new PokemonMetadata(1, "Ivysaur", 156, 158, 120);
+
+	    when(mockPokemonMetadataProvider.getPokemonMetadata(1)).thenReturn(pokemonEspece);
+	    PokemonMetadata pokemonMetaData = mockPokemonMetadataProvider.getPokemonMetadata(pokemonEspece.getIndex());
+
+	    verify(mockPokemonMetadataProvider).getPokemonMetadata(pokemonEspece.getIndex());
+	    assertEquals(pokemonEspece.getDefense(), pokemonMetaData.getDefense());
+	}
+
+	@Test
+	public void validatePokemonMetadataEqualityTest_getStamina() throws PokedexException {
+	    PokemonMetadata pokemonEspece = new PokemonMetadata(2, "Venusaur", 198, 200, 160);
+
+	    when(mockPokemonMetadataProvider.getPokemonMetadata(2)).thenReturn(pokemonEspece);
+	    PokemonMetadata pokemonMetaData = mockPokemonMetadataProvider.getPokemonMetadata(pokemonEspece.getIndex());
+
+	    verify(mockPokemonMetadataProvider).getPokemonMetadata(pokemonEspece.getIndex());
+	    assertEquals(pokemonEspece.getStamina(), pokemonMetaData.getStamina());
+	}
+
+	@Test
+	public void validatePokemonMetadataForInvalidIndex() {
+	    assertThrows(PokedexException.class, () -> {
+	        when(mockPokemonMetadataProvider.getPokemonMetadata(-1))
+	            .thenThrow(new PokedexException("Invalid Pokemon index"));
+	        mockPokemonMetadataProvider.getPokemonMetadata(-1);
+	    });
+	}
+
+	@Test
+	public void validatePokemonMetadata_NameConsistency() throws PokedexException {
+	    PokemonMetadata pokemonEspece = new PokemonMetadata(3, "Charmander", 128, 108, 78);
+
+	    when(mockPokemonMetadataProvider.getPokemonMetadata(3)).thenReturn(pokemonEspece);
+	    PokemonMetadata pokemonMetaData = mockPokemonMetadataProvider.getPokemonMetadata(pokemonEspece.getIndex());
+
+	    verify(mockPokemonMetadataProvider).getPokemonMetadata(pokemonEspece.getIndex());
+	    assertEquals(pokemonEspece.getName(), pokemonMetaData.getName());
+	}
+
+	@Test
+	public void validatePokemonMetadata_IndexConsistency() throws PokedexException {
+	    PokemonMetadata pokemonEspece = new PokemonMetadata(4, "Squirtle", 112, 142, 88);
+
+	    when(mockPokemonMetadataProvider.getPokemonMetadata(4)).thenReturn(pokemonEspece);
+	    PokemonMetadata pokemonMetaData = mockPokemonMetadataProvider.getPokemonMetadata(pokemonEspece.getIndex());
+
+	    verify(mockPokemonMetadataProvider).getPokemonMetadata(pokemonEspece.getIndex());
+	    assertEquals(pokemonEspece.getIndex(), pokemonMetaData.getIndex());
+	}
+
+	@Test
+	public void validatePokemonMetadataForLargeIndex() {
+	    assertThrows(PokedexException.class, () -> {
+	        when(mockPokemonMetadataProvider.getPokemonMetadata(9999))
+	            .thenThrow(new PokedexException("Invalid Pokemon index"));
+	        mockPokemonMetadataProvider.getPokemonMetadata(9999);
+	    });
+	}
+
 }
